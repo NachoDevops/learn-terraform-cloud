a57c47e33ab6b0e747e378f9e435a44e45a02646
Nacho Borente <i.borentegonzalez@accenture.com>
2023-05-30 13:34:29 +0200

    Remove Cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "TCloudTutorial"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
